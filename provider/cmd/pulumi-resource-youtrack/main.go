3f2ab4420cb45b45fc0440331ba2b497f04ce818
Marc Ende <me@e-beyond.de>
2024-03-19 13:00:21 +0000

    fix: naming

--- provider/cmd/pulumi-resource-youtrack/main.go
+++ provider/cmd/pulumi-resource-youtrack/main.go
@@ -17,8 +17,8 @@ package main
 import (
 	p "github.com/pulumi/pulumi-go-provider"
 
-	xyz "github.com/pulumi/pulumi-xyz/provider"
+	youtrack "github.com/oss4u/pulumi-youtrack-native/provider"
 )
 
 // Serve the provider against Pulumi's Provider protocol.
-func main() { p.RunProvider(xyz.Name, xyz.Version, xyz.Provider()) }
+func main() { p.RunProvider(youtrack.Name, youtrack.Version, youtrack.Provider()) }
